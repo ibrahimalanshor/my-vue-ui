bc8ad6e9b112ee0279344a142e8496cf42ffc710
ibra <ibrahimalanshor6@gmail.com>
2022-09-05 14:22:10 +0800

    add badge

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
-import { TestAlert, TestCollapse } from '@/test';
+import { TestAlert, TestCollapse, TestBadge } from '@/test';
 </script>
 
 <template>
   <div class="p-5">
     <test-alert />
     <test-collapse />
+    <test-badge />
   </div>
 </template>
---
+++ src/components/base-badge.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    default: 'default',
+  },
+});
+
+const badgeClass = computed(() => {
+  const bgColors = {
+    default: 'bg-gray-100',
+    primary: 'bg-blue-100',
+    success: 'bg-green-100',
+    warning: 'bg-yellow-100',
+    error: 'bg-red-100',
+  };
+
+  const textColors = {
+    default: 'text-gray-800',
+    primary: 'text-blue-800',
+    success: 'text-green-800',
+    warning: 'text-yellow-800',
+    error: 'text-red-800',
+  };
+
+  const bgClass = bgColors[props.color] ?? bgColors.default;
+  const textClass = textColors[props.color] ?? textColors.default;
+
+  return [bgClass, textClass];
+});
+</script>
+
+<template>
+  <span class="text-xs font-semibold mr-2 px-2.5 py-0.5" :class="badgeClass">{{
+    props.label
+  }}</span>
+</template>
