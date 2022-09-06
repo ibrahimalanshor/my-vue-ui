393005f34e71d270555fa632f05447e6f719add6
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-09-06 22:01:18 +0700

    add checkbox

---
+++ src/test/test-checkbox.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { reactive } from 'vue';
+import { BaseCheckbox } from '@/components';
+
+const checkbox = reactive({
+  model: false,
+});
+
+const handleChange = () => console.log('onchange');
+</script>
+
+<template>
+  <div>
+    {{ checkbox.model }}
+    <base-checkbox
+      label="namun"
+      id="test-checkbox"
+      v-model="checkbox.model"
+      v-on:change="handleChange"
+    ></base-checkbox>
+  </div>
+</template>
